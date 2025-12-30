399d6da560aab205bd25f2e81bedde65279754a6
Alexandre Lugand <alexandre.lugand@gmail.com>
2025-12-30 23:32:54 +0100

    feat: ajout du nuget log4net pour gérer les traces (logs) feat: ajout d'un gestionnaire d'exception dans le serveur (try / catch)

--- Client/ChatClient.cs
+++ Client/ChatClient.cs
@@ -10,16 +10,17 @@ namespace Client
             const string serverIp = "127.0.0.1";
             const int serverPort = 5000;
 
-            using (var tcpClient = new TcpClient())
+            using (var tcpClient = new TcpClient()) //Socket (point d'entrée du réseau / porte d'entrée / sortie vers le réseau)
             {
-                tcpClient.Connect(serverIp, serverPort);
+                tcpClient.Connect(serverIp, serverPort); //Etablissement de la connexion avec le serveur
                 Console.WriteLine("Connecté au serveur de chat.");
 
-                var stream = tcpClient.GetStream();
+                //Flux réseau
+                var stream = tcpClient.GetStream(); //Canal ce communication bi-directionnel => Read / Write (forme de buffer réseau)
 
                 while (true)
                 { 
-                    var msg = Console.ReadLine();
+                    var msg = Console.ReadLine(); //Lecture de la chaine de caractère de la console (entréé par l'utilisateur)
 
                     if (string.IsNullOrEmpty(msg))
                         continue;
@@ -27,14 +28,14 @@ namespace Client
                     if (string.Compare(msg, "exit", StringComparison.OrdinalIgnoreCase) == 0) //msg == "exit" ou msg == "EXIT"
                         break;
 
-                    byte[] data = Encoding.ASCII.GetBytes(msg);
-                    stream.Write(data, 0, data.Length);
-                    stream.Flush();
+                    byte[] data = Encoding.ASCII.GetBytes(msg); //Converti la chaine de caractère en tableau d'octets
+                    stream.Write(data, 0, data.Length); //J'écris le tableau d'octets dans le stream réseau => sera envoyé au serveur
+                    stream.Flush(); // => Maintenant, je l'envoie dans le réseau
 
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine($"Serveur: {response}");
+                    byte[] buffer = new byte[1024]; //J'alloue un buffer d'octets de 1024 octets
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length); //Je lis les octets disponibles dans le stream (bloque s'il n'y en a pas), retourne le nombre d'octets lus
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead); //Je transforme les octets en une chaine de caractères lisibles
+                    Console.WriteLine($"Serveur: {response}"); // Et je l'affiche dans la console
                 }
             }
         }
--- Server/ChatServer.cs
+++ Server/ChatServer.cs
@@ -1,3 +1,5 @@
+using log4net;
+using log4net.Config;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,22 +9,41 @@ namespace MidChat.Server // Espace de noms
     internal class ChatServer : System.Object
     {
         private static List<TcpClient> TcpClients = new List<TcpClient>();
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ChatServer));
+
+        public ChatServer()
+        {
+            //Pour gérer la couleur dans la console (obligatoire)
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var logRepository = LogManager.GetRepository();
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config")); //Cherche le fichier de configuration log4net dans le répertoire où se trouve l'exécutable (tjs mettre Copy always sur le fichier log4net.config)
+
+            Log.Info("ChatServer initialized.");
+        }
 
         public void Run() //Methode
         {
-            const int port = 5000;
-            TcpListener listener = new TcpListener(IPAddress.Any, port);
-            listener.Start();
-            Console.WriteLine($"Serveur démarré sur le port {port}");
-
-            while (true)
+            try
             {
-                TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Client connecté");
-                TcpClients.Add(client);
+                const int port = 5000;
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                Log.Info($"Serveur démarré sur le port {port}");
 
-                var thread = new Thread(HandleClient);
-                thread.Start(client);
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Log.Warn("Client connecté");
+                    TcpClients.Add(client);
+
+                    var thread = new Thread(HandleClient);
+                    thread.Start(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("Erreur inattendue", e);
             }
         }
 
@@ -30,31 +51,39 @@ namespace MidChat.Server // Espace de noms
         {
             if (obj is TcpClient tcpClient)
             {
-                using (tcpClient)
+                try
                 {
-                    var stream = tcpClient.GetStream();
-                    byte[] buffer = new byte[1024];
-
-                    while (true)
+                    using (tcpClient)
                     {
-                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                        if (bytesRead == 0)
-                            break; //Déconnexion du client
+                        var stream = tcpClient.GetStream();
+                        byte[] buffer = new byte[1024];
 
-                        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                        Console.WriteLine($"Reçu: {message}");
-
-                        foreach (var client in TcpClients)
+                        while (true)
                         {
-                            if (client == tcpClient)
-                                continue; //Ne pas renvoyer au client émetteur
+                            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                            if (bytesRead == 0)
+                                break; //Déconnexion du client
 
-                            var clientStream = client.GetStream();
-                            byte[] data = Encoding.ASCII.GetBytes(message);
-                            clientStream.Write(data, 0, data.Length);
-                            clientStream.Flush();
+                            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                            Log.Debug($"Reçu: {message}");
+
+                            foreach (var client in TcpClients)
+                            {
+                                if (client == tcpClient)
+                                    continue; //Ne pas renvoyer au client émetteur
+
+                                var clientStream = client.GetStream();
+                                byte[] data = Encoding.ASCII.GetBytes(message);
+                                clientStream.Write(data, 0, data.Length);
+                                clientStream.Flush();
+                            }
                         }
                     }
+                }
+                catch (Exception e)
+                {
+                    Log.Error("Erreur inattendue", e);
+                    TcpClients.Remove(tcpClient);
                 }
             }
         }
